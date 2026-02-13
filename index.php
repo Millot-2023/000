6c08d3c2ca083db9adbccd6e0b3168b4d1fdcc76
Christophe Millot <cmillot2004@gmail.com>
2026-02-13 21:05:59 +0100

    genese

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+include 'src/includes/_head.php'; 
+include 'src/includes/_header.php'; 
+?>
+
+<main>
+    <?php include 'src/partials/_home.php'; ?>
+</main>
+
+<?php 
+include 'src/includes/_footer.php'; 
+?>
---
+++ src/includes/_head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CMS-2026 V4.0</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
